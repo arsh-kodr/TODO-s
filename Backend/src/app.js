d5997e24ae0c2f4e4784f52edf0d0a47b391d3e9
arsh-kodr <raisahab2364@gmail.com>
2025-09-14 18:43:48 +0530

    Add initial frontend setup with Vite and React
    - Created index.html for the frontend entry point.
    - Added Vite logo SVG for favicon.
    - Updated app.js to serve static files and handle routing to index.html.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,21 +4,27 @@ const authRoutes = require("./routes/auth.routes");
 const todoRoutes = require("./routes/todo.routes")
 const aiRoutes = require("./routes/ai.routes")
 const cors = require("cors");
+const path = require("path")
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join("public")))
 
 app.use(
   cors({
-    origin: "http://localhost:5173",  // your frontend URL
-    credentials: true,                // allow cookies/headers
+    origin: "http://localhost:5173", 
+    credentials: true,                
   })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/todo" , todoRoutes);
 app.use("/ai" ,aiRoutes);
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
 
 module.exports = app;
